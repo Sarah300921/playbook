5923ca9be8036bd0381e82a0ba4778e0061bd2b1
Sarah300921 <cv6025@innovaccion.mx>
2022-04-21 17:31:24 -0500

    Proyecto antes TDD

---
+++ weekly_mission_3/Proyecto_01/app/missionC.js
@@ -0,0 +1,8 @@
+// JavaScript source code
+class MC {
+    constructor(nombre) {
+        this.nombre = nombre
+    }
+}
+
+module.exports = MC
---
+++ weekly_mission_3/Proyecto_01/index.js
@@ -0,0 +1,10 @@
+// JavaScript source code
+const MissionCommander = require('./app/missionC');
+const carlo = new MissionCommander("Carlo")
+console.log(carlo.nombre)
+
+const Woopa = new MissionCommander("Woopa")
+console.log(Woopa.nombre)
+
+const Tulio = new MissionCommander("Tuly")
+console.log(Tulio.nombre)
