4b7c7bfbb6ee2b825fd064094997ddd78f3399bf
Sarah300921 <cv6025@innovaccion.mx>
2022-05-02 20:13:02 -0500

    agregando route y params

--- weekly_mission_3/express_basic/apps.js
+++ weekly_mission_3/express_basic/apps.js
@@ -16,8 +16,13 @@ app.get('/node', (req, res) => {
     const explorer2 = { id: 2, name: "explorerDos" }
     const explorer3 = { id: 3, name: "explorerTres" }
     res.send([explorer1.name, explorer2.id, explorer3.name])
-
 })
+
+app.get('/explorers/:test', (req,res)=> {
+    console.log(req.param)
+    res.send(req.params)
+})
+
 app.listen(port,() => {
     console.log("Server running")
 })
