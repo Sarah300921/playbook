32d963857dbac0fdfbbaf025fd4fcd2854df422b
Sarah300921 <cv6025@innovaccion.mx>
2022-05-02 19:52:43 -0500

    express basic done

--- weekly_mission_3/express_basic/apps.js
+++ weekly_mission_3/express_basic/apps.js
@@ -8,6 +8,19 @@ app.get('/', (req, res) => {
     res.send("HOLA MUNDO")
 })
 
+app.get('/launchx', (req, res) => {
+    res.send("otro Hola Mundo")
+})
+app.get('/node', (req, res) => {
+    const explorer1 = { id: 1, name: "explorerUno" }
+    const explorer2 = { id: 2, name: "explorerDos" }
+    const explorer3 = { id: 3, name: "explorerTres" }
+    res.send([explorer1.name, explorer2.id, explorer3.name])
+
+})
 app.listen(port,() => {
     console.log("Server running")
 })
+
+
+
