070e114f47a89c5d3a17207ddb793282cf3971e2
Sarah300921 <cv6025@innovaccion.mx>
2022-05-06 14:38:11 -0500

    FIZZBUZZ refactorizacion V1.0

--- weekly_mission_4/FIZZBUZZ/index.js
+++ weekly_mission_4/FIZZBUZZ/index.js
@@ -1,14 +1,12 @@
 // JavaScript source code
 const Reader = require("./lib/utils/Reader")
 const ExplorerService = require("./lib/services/ExplorerService")
+const FizzBuzzService = require("./lib/services/FizzBuzzService")
+
 const explorers = Reader.readJsonFile("explorers.json")
 
 console.log(ExplorerService.porMision(explorers, "node"))
 console.log(ExplorerService.cantidadPorMision(explorers, "node"))
-console.log(ExplorerService.usernamePorMision(explorers,"node"))
+console.log(ExplorerService.usernamePorMision(explorers, "node"))
 
-//ExplorerService(explorers, "node");
-//ExplorerService(explorers, "node");
-
-//console.log(explorers)
-
+FizzBuzzService.validacionExplorerScore(explorers, "node")
---
+++ weekly_mission_4/FIZZBUZZ/lib/services/FizzBuzzService.js
@@ -0,0 +1,27 @@
+// JavaScript source code
+class FizzbuzzService {
+    static validacionExplorerScore(explorers,mission) {
+        const validacionExplorerMision = explorers.filter((explorer) => explorer.mission === mission)
+        const validation = validacionExplorerMision.map((explorer) => {
+            if (explorer.score % 5 === 0 && explorer.score % 3 === 0) {
+                explorer.trick = "FIZZBUZZ";
+                console.log(explorer.name + " " + "FIZZBUZZ")
+                return explorer;
+            } else if (explorer.score % 3 === 0) {
+                explorer.trick = "FIZZ";
+                console.log(explorer.name + " " + "FIZZ")
+                return explorer;
+            } else if (explorer.score % 5 === 0) {
+                explorer.trick = "BUZZ";
+                console.log(explorer.name + " " + "BUZZ")
+                return explorer;
+            } else {
+                explorer.trick = explorer.score;
+                console.log(explorer.name + " " + explorer.score)
+                return explorer;
+            }           
+        })
+    }
+}
+
+module.exports = FizzbuzzService
